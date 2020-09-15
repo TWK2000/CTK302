2bb9e106902274217e5215cce74d13e709abd580
TWK2000 <58342463+TWK2000@users.noreply.github.com>
2020-09-15 11:47:23 -0500

    Food

--- p5/Unit2/anim1/sketch.js
+++ p5/Unit2/anim1/sketch.js
@@ -1,4 +1,5 @@
 var x = 0 ;
+
 function setup() {
   createCanvas(500, 500);
 }
@@ -6,6 +7,12 @@ function setup() {
 function draw() {
 background(100);
 
-rect(0, 250, 50, 50);
-x = x = 1
+
+text("Tahj The Libra", x, 250);
+//rect(x, 250, 50, 50);
+x = x + 3
+if (x > width) {
+  x = 0 ;
+}
+
 }
---
+++ p5/Unit2/anim2/sketch.js
@@ -0,0 +1,80 @@
+var x = 0 ;
+
+function setup() {
+  createCanvas(500, 500);
+}
+
+function draw() {
+background(100);
+
+push() ;
+translate(x, 0) ;
+//rect(0, 250, 50, 50) ;
+avatar();
+x = x + 5
+
+if (x > width) {
+  x = -300 ;
+}
+
+pop() ;
+
+}
+
+
+function avatar() {
+  if (mouseIsPressed) {
+   // background(138, 119, 106);
+  } else {
+    background(201, 129, 77)
+  }
+
+  //headphones
+  if (mouseIsPressed){
+  fill('black');
+  arc(25, 199, 80, 80, PI/2, PI + PI/2, CHORD);
+
+  noStroke();
+  arc(312, 199, 80, 80, PI + PI/2, PI/2, CHORD);
+
+
+  arc(170, 159, 290, 200, PI, 0, CHORD);
+
+  fill(201, 129, 77);
+  arc(170, 170, 290, 200, PI, 0, CHORD);
+  }
+  //eyes
+  fill('white')
+  ellipse(100,196,110,100); //eye 1
+
+  fill('brown')
+  ellipse(100,196,50,50); //eye 1 pupil
+
+  fill('black')
+  ellipse(100, 196, 20, 20); //eye 1 retna
+
+  fill('white')
+  ellipse(218, 196, 110, 100); //eye 2
+
+  fill('brown')
+  ellipse(218, 196, 50,50); //eye 2 pupil
+
+  fill('black')
+  ellipse(218, 196, 20, 20); //eye 2 retna
+
+
+  let c = color(245, 180, 110);
+fill(c);
+  strokeWeight(1);
+stroke(51);
+  rect(140, 297, 45, 45);
+
+
+
+
+
+  fill('white')
+  text(mouseX + ', ' + mouseY,20,20);
+
+  print(mouseIsPressed);
+}
