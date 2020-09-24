728c379e339411052311ddf0f3583895fb396c9e
TWK2000 <58342463+TWK2000@users.noreply.github.com>
2020-09-24 17:54:53 -0500

    Style

---
+++ p5/Unit2/stoplight/sketch.js
@@ -0,0 +1,84 @@
+let state = 0;
+let timer = 0;
+let x = 0;
+let velocity = 10;
+
+function setup() {
+  createCanvas(800, 800);
+  ellipse(CENTER);
+  rectMode(CENTER);
+
+}
+
+function draw() {
+  background(100);
+  fill('yellow');
+  rect(width / 2, height / 2, 200, 600);
+
+  // car code
+  fill('blue') ;
+  rect(x, 750, 75, 20) ;
+  x = x + velocity ;
+  if (x > width) {
+    x = 0 ;
+  }
+
+  ellipse(width / 2, height / 2 - 170, 150, 150);
+  ellipse(width / 2, height / 2, 150, 150);
+  ellipse(width / 2, height / 2 + 170, 150, 150);
+  switch (state) {
+
+    case 0: //red
+      text("0", 250, 250);
+      fill('red');
+      ellipse(width / 2, height / 2 - 170, 150, 150);
+      fill('grey')
+      ellipse(width / 2, height / 2, 150, 150);
+      ellipse(width / 2, height / 2 + 170, 150, 150);
+      velocity = 0;
+      break;
+
+    case 1: //green
+      text("1", 250, 250);
+      fill('grey');
+      ellipse(width / 2, height / 2 - 170, 150, 150);
+      ellipse(width / 2, height / 2, 150, 150);
+      fill('green');
+      ellipse(width / 2, height / 2 + 170, 150, 150);
+      velocity = 10;
+      break;
+
+    case 2: //yellow
+      text("2", 250, 250);
+      fill('grey');
+      ellipse(width / 2, height / 2 - 170, 150, 150);
+      fill('yellow');
+      ellipse(width / 2, height / 2, 150, 150);
+      fill('grey');
+      ellipse(width / 2, height / 2 + 170, 150, 150);
+      velocity = 5;
+      break;
+
+
+
+
+
+
+  }
+
+  timer = timer + 1;
+  if (timer > 100) {
+    timer = 0;
+    state = state + 1;
+    if (state > 2) state = 0;
+  }
+
+
+}
+
+function mouseReleased() {
+  state = state + 1;
+  if (state > 2) {
+    state = 0;
+  }
+}
